f6cc2b6e114e3d96b695ebb807a047f86850492d
PhilipGooch <philip.daniel.gooch@gmail.com>
2020-02-04 15:17:39 +0000

    Added smoke despawn.

---
+++ ScienceCombat/Assets/Scripts/Smoke.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smoke : MonoBehaviour
+{
+    [SerializeField] private float radius;
+    [SerializeField] private float duration;
+    private float spawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //spawnTime = Time.time;
+        StartCoroutine(DEATH());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Time.time > spawnTime + duration)
+        //{
+            
+        //    GetComponentInParent<SmokeAttack>().smokeExists = false;
+
+        //    Destroy(gameObject);
+        //}
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    IEnumerator DEATH()
+    {
+        
+        yield return new WaitForSeconds(duration);
+
+        Destroy(gameObject);
+    }
+}
