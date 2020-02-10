f2a8a28e89ae7bd03e471ec5456e013c90ecfe3a
Jamie <1703169@uad.ac.uk>
2020-02-10 16:35:01 +0000

    Testing Prefab values

--- ScienceCombat/Assets/Scripts/Laser.cs
+++ ScienceCombat/Assets/Scripts/Laser.cs
@@ -11,13 +11,7 @@ public class Laser : MonoBehaviour
     [SerializeField] private float radius;
     LineRenderer line;
     [SerializeField] private string playerFire;
-    [SerializeField] private PlayerController pc;
-
-    [SerializeField] private PlayerSelector playerSelector;
-
-
-
-
+    [SerializeField] private Scientist scientist;
 
 
     void Start()
@@ -28,44 +22,42 @@ public class Laser : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        if (pc.isCaptured) return;
+        if (scientist.isCaptured) return;
 
 
-        if (playerSelector.targetedPlayer == 1)
+        if (Mathf.Abs(Input.GetAxis(playerFire)) > 0.01f)
         {
-            if (Mathf.Abs(Input.GetAxis(playerFire)) > 0.01f)
+            RaycastHit hit;
+            Debug.DrawRay(transform.position, transform.forward * magnitude, Color.green, .1f);
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit, magnitude))
             {
-                RaycastHit hit;
-                Debug.DrawRay(transform.position, transform.forward * magnitude, Color.green, .1f);
 
-                if (Physics.Raycast(transform.position, transform.forward, out hit, magnitude))
-                {
-
-                    if (hit.collider != null)
-                    {
-
-                        line.enabled = true;
-                        line.transform.localScale = new Vector3(radius, radius, Mathf.Lerp(0, hit.distance, 1f));
-                        print("Collided");
-                        GameObject enemy = hit.collider.gameObject;
-                        if (enemy != null)
-                        {
-                            print("Hit GameObject");
-                            // Destroy(enemy);
-                        }
-                    }
-                }
-                else
+                if (hit.collider != null)
                 {
 
                     line.enabled = true;
-                    line.transform.localScale = new Vector3(radius, radius, Mathf.Lerp(0, magnitude, 1f));
+                    line.transform.localScale = new Vector3(radius, radius, Mathf.Lerp(0, hit.distance, 1f));
+                    print("Collided");
+                    GameObject enemy = hit.collider.gameObject;
+                    if (enemy != null)
+                    {
+                        print("Hit GameObject");
+                        // Destroy(enemy);
+                    }
                 }
             }
             else
             {
-                line.enabled = false;
+
+                line.enabled = true;
+                line.transform.localScale = new Vector3(radius, radius, Mathf.Lerp(0, magnitude, 1f));
             }
         }
+        else
+        {
+            line.enabled = false;
+        }
+
     }
 }
