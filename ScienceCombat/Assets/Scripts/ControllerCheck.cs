e1ae11abad8da9ed170cf554a325815854fe329f
Jamie <1703169@uad.ac.uk>
2020-02-13 14:18:31 +0000

    Added Contoller Select script

---
+++ ScienceCombat/Assets/Scripts/ControllerCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCheck : MonoBehaviour
+{
+    public string[] names = Input.GetJoystickNames();
+
+    private void Start()
+    {
+        names = Input.GetJoystickNames();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (names != Input.GetJoystickNames()) {
+            names = Input.GetJoystickNames();
+        }
+        int i = 0;
+        // if(Input.GetKeyDown(input))
+        foreach (var item in names)
+        {
+            i++;
+            if (Input.GetKeyDown($"joystick {i} button 0")) {
+                print($"joystick {i} button 0");
+            }
+        }
+    }
+}
