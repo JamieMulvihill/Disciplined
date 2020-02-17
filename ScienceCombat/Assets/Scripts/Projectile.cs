20d52fec6d4db0c6eea686e406a0351f37e932f6
PhilipGooch <philip.daniel.gooch@gmail.com>
2020-02-17 11:57:40 +0000

    Changed ontriggerEnter function in projectile

--- ScienceCombat/Assets/Scripts/Projectile.cs
+++ ScienceCombat/Assets/Scripts/Projectile.cs
@@ -36,14 +36,10 @@ public class Projectile : MonoBehaviour
 
     private void OnTriggerStay(Collider collision)
     {
-        hitObjecets = Physics.OverlapSphere(transform.position, damageRadius);
-        foreach (Collider hit in hitObjecets)
+        
+        if (collision.gameObject.tag != gameObject.tag)
         {
-
-            if (hit.tag != gameObject.tag)
-            {
-                AreaOfEffect(hit.gameObject);
-            }
+            AreaOfEffect(gameObject);
         }
 
     }
