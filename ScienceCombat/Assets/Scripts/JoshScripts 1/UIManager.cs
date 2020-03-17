4b7e880daa98567218026e34933fa2cf000ce9cb
JOSH MARSH <1800927@uad.ac.uk>
2020-03-17 13:52:55 +0000

    UI progress

---
+++ ScienceCombat/Assets/Scripts/JoshScripts 1/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public bool cooldown1;
+    public bool cooldown2;
+
+    void Start()
+    {
+        cooldown1 = false;
+        cooldown2 = false;
+    }
+
+    void Update()
+    {
+
+    }
+}
+
+/*Get number of players in the game
+ * 
+ * create as many UI's as needed
+ * 
+ * Assign players to created UI
+ */
