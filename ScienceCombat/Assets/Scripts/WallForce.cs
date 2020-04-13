3e3eb98018720ee5995e742105979b913b6ac7b8
JamieMulvihill <44208828+JamieMulvihill@users.noreply.github.com>
2020-04-13 17:19:43 +0100

    Adding force to knock players of walls when zone quaratined

---
+++ ScienceCombat/Assets/Scripts/WallForce.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallForce : MonoBehaviour
+{
+    public float xAxisForce;
+    public float zAxisForce;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Scientist>()) {
+            other.GetComponent<Rigidbody>().AddForce(new Vector3(xAxisForce, 0, zAxisForce), ForceMode.Impulse);
+        }
+    }
+}
