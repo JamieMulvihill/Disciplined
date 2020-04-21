bbfecbbd7416ccc7acd60857dbb7337945e5a11a
JamieMulvihill <44208828+JamieMulvihill@users.noreply.github.com>
2020-04-21 14:01:50 +0100

    Added Script to lock rotation of Health UI

---
+++ ScienceCombat/Assets/Scripts/HealthUIRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUIRotation : MonoBehaviour
+{
+    public bool useFixedRotation = true;
+    private Quaternion fixedRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fixedRotation = transform.parent.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (useFixedRotation) {
+            transform.rotation = fixedRotation;
+        }
+    }
+}
