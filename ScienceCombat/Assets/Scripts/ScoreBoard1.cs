28c5cc38aee87fb713f4b3980af97a0db940e1c1
Philip Gooch <phlip.daniel.gooch@gmail.com>
2020-04-22 01:43:07 +0100

    Added health bar

---
+++ ScienceCombat/Assets/Scripts/ScoreBoard1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBoard1 : MonoBehaviour
+{
+    int [] scores;
+    Scientist[] scientists;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scores = new int[4];
+        scientists = new Scientist[4];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
