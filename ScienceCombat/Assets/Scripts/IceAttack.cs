f88ac112b7fd645eb40ffec1706b278884bcbc36
Philip Gooch <phlip.daniel.gooch@gmail.com>
2020-04-23 18:42:46 +0100

    added overheat to chemist ice attack

--- ScienceCombat/Assets/Scripts/IceAttack.cs
+++ ScienceCombat/Assets/Scripts/IceAttack.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class IceAttack : MonoBehaviour
 {
+    
+    [Header("Overheat values")]
+    public Overheat overheat;
+    [SerializeField] protected float heatPerUse = 0f;
+    [SerializeField] protected float cooloffPerSecond = 0f;
+    [SerializeField] protected float chillThreshold = 100f;
+    [Header("Other Settings")]
     public string triggerButton;
-
     public GameObject icePrefab;
     private GameObject ice;
 
@@ -13,6 +19,7 @@ public class IceAttack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        overheat = new Overheat();
         triggerButton += GetComponent<Scientist>().controllerIndex.ToString();
     }
 
@@ -24,8 +31,13 @@ public class IceAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Abs(Input.GetAxis(triggerButton)) > 0.01f && !ice && GetComponent<Scientist>().isCaptured == false)
+        overheat.chillThreshold = chillThreshold;
+        overheat.Chill(cooloffPerSecond * Time.deltaTime);
+
+        if (Mathf.Abs(Input.GetAxis(triggerButton)) > 0.01f && GetComponent<Scientist>().isCaptured == false && overheat.GetOverheated() == false)
         {
+            overheat.Broil(heatPerUse);
+
             Vector3 SpawnPoint = transform.position;
 
             ice = Instantiate(icePrefab, SpawnPoint, Quaternion.LookRotation(transform.forward, transform.up));
